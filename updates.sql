ac3c539ff18da615fc245208d1e329a1fbbbd24a
Sophia <46500033+sophiaxuu@users.noreply.github.com>
2021-04-13 22:56:24 -0400

    Update updates.sql

--- updates.sql
+++ updates.sql
@@ -4,7 +4,7 @@
 
 INSERT INTO Hospital VALUES
 ('1000', 'ALB', 'North America', 'US', '2020-12-13', 48530, 788, 1003, 14, 16863.576, 273.82, 206292, 71.684, 0.314, NULL, 2.89), 
-('69676','CHE', 'Europe', 'Switzerland', '2020-10-19', 83159, 8737, 2138, 15, 9608.627, 1009.519, 1278530, 147.728, 0.163, NULL, 4.53); 
+('6999','CHE', 'Europe', 'Switzerland', '2020-10-19', 83159, 8737, 2138, 15, 9608.627, 1009.519, 1278530, 147.728, 0.163, NULL, 4.53); 
 
 
 -- INSERTION To HospitalLocatedIn --- 
@@ -38,7 +38,7 @@ WHERE country_name = 'Switzerland';
 --- Insert to Covid19RelatedSpending (only US since Spending Data is US only)--- 
 
 INSERT INTO Covid19RelatedSpending VALUES
-('110', 1, 15, 13, -0.00277, 0.141, 0.00596, -0.0207, -0.0101, 0.026, -0.0116, -0.00348, 0.021, 0.00762);
+('110', 1, 10, 13, -0.00277, 0.141, 0.00596, -0.0207, -0.0101, 0.026, -0.0116, -0.00348, 0.021, 0.00762);
 INSERT INTO Covid19RelatedSpendingByCountry VALUES 
 ('110', 'US'); 
 
