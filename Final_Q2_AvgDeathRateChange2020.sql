38d41e01e7b1aa7b14333a0bb25664c731338ca8
sophiaxuu <fxu13@jhu.edu>
2021-05-10 23:24:58 -0400

    q2

---
+++ Final_Q2_AvgDeathRateChange2020.sql
@@ -0,0 +1,20 @@
+--- Final_Q2_AvgDeathRateChange2020.sql stored procedure 
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS AvgDeathRateChange2020 // 
+
+CREATE PROCEDURE AvgDeathRateChange2020() 
+BEGIN 
+    SELECT AVG2020.avg_2020 - AVG2021.avg_2021 
+	FROM
+        (SELECT AVG(total_cases_per_million) AS avg_2020
+        FROM Hospital 
+        WHERE DATEDIFF('2020-12-30', date) > 0) AS AVG2020, 
+        (SELECT AVG(total_cases_per_million) AS avg_2021
+        FROM Hospital 
+        WHERE DATEDIFF('2020-12-30', date) < 0) AS AVG2021; 
+END; // 
+
+
+DELIMITER ; 
