d5d2de95d04c1e17487024f916fa81b44edb50a6
Fanbo Xu <fxu13@jh.edu>
2021-05-10 22:02:19 -0400

    q1 fail

--- Final_Q1_TotalCaseByContinent.sql
+++ Final_Q1_TotalCaseByContinent.sql
@@ -1,15 +1,14 @@
 --- Final_Q1_TotalCaseByContinent.sql stored procedure 
---- You may have to call: mysql -h dbase.cs.jhu.edu -u 21sp_fxu13 -p < Final_Q1_TotalCaseByContinent.sql
-DELIMITER //
 
+DELIMITER //
 
 DROP PROCEDURE IF EXISTS TotalCaseByContinent // 
 
 CREATE PROCEDURE TotalCaseByContinent() 
 BEGIN 
     SELECT continent, SUM(total_cases_per_million) AS total_cases
-	FROM Hospital 
-	GROUP BY continent;
+    FROM Hospital
+    GROUP BY continent;
 END; // 
 
 
