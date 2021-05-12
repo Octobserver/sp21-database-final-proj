1059bad6ce04cb4bf137edbb0bf7ff9039544c4b
sophiaxuu <fxu13@jhu.edu>
2021-05-11 22:12:56 -0400

    insertion

---
+++ Final_Hospital_Insertion.php
@@ -0,0 +1,27 @@
+
+<head><title> Adding report: Insert Data To Hospital Dataset </title> </head>
+<body>
+<?php
+    //open a connection to dbase server 
+    include 'open.php'; 
+
+    echo "<h2>Adding report: Insert Data To Hospital Dataset</h2><br>";
+
+    echo "<br><br>"; 
+
+    // $sql = "DELETE FROM Hospital WHERE reportID =".$_POST['input_reportID'];
+    // $sql = "DELETE FROM Hospital WHERE reportID = '$input_reportID' ";
+    // $sql = "DELETE FROM Hospital WHERE reportID = '$input_reportID' ";
+    // $sql = "DELETE FROM Hospital WHERE reportID = '10' ";
+    // INSERT INTO Hospital VALUES
+    $sql = "INSERT INTO Hospital VALUES ('6999','CHE', 'Europe', 'Switzerland', '2020-10-19', 83159, 8737, 2138, 15, 9608.627, 1009.519, 1278530, 147.728, 0.163, NULL, 4.53)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error inserting record: " . $conn->error;
+    }
+    $conn->close(); 
+?>
+
+</body>
