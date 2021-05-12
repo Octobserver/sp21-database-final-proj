2e78a5c41af69a642f083e176909ec3c0d6f483e
sophiaxuu <fxu13@jhu.edu>
2021-05-11 22:21:09 -0400

    x

--- Final_Hospital_Insertion.php
+++ Final_Hospital_Insertion.php
@@ -13,7 +13,7 @@
     // $sql = "DELETE FROM Hospital WHERE reportID = '$input_reportID' ";
     // INSERT INTO Hospital VALUES
     // $sql = "INSERT INTO Hospital VALUES ('6999','CHE', 'Europe', 'Switzerland', '2020-10-19', 83159, 8737, 2138, 15, 9608.627, 1009.519, 1278530, 147.728, 0.163, NULL, 4.53)";
-    $sql = "INSERT INTO Hospital VALUES".$_POST['inputs'];
+    $sql = "INSERT INTO Hospital VALUES(".$_POST['inputs'].);
 
     if ($conn->query($sql) === TRUE) {
         echo "Record inserted successfully";
