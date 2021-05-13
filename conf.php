90411a484edc42220a2b2ae9b6d93ba70075fd06
Sophia <46500033+sophiaxuu@users.noreply.github.com>
2021-05-12 20:36:21 -0400

    Delete conf.php

--- conf.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	//these are credentials which will be used by open.php to create connection to dbase
-	//make updates here as noted below, and save this file as conf.php
-	$dbhost = "dbase.cs.jhu.edu"; // database host
-	$dbuser = "21sp_fxu13";   
-	$dbpass = "FBYylLkFzw";            
-	$dbname = "21sp_fxu13_db"; 
-?>
